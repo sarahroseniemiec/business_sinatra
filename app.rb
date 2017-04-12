1c256254513a265f196fbc8afcb40c65a763d7b5
sarah niemiec <sroseniemiec@gmail.com>
2017-04-12 15:18:10 -0400

    contact page

--- app.rb
+++ app.rb
@@ -1,4 +1,5 @@
 require "sinatra"
+require "sendgrid-ruby"
 
 get "/" do
   erb :home
@@ -14,4 +15,28 @@ end
 
 get "/about" do
   erb :about
+end
+
+post "/contact_me_response" do
+  # if you want to see what params have been sent
+  #   from your form uncomment the following line
+  params.inspect
+
+  # you can use these params as input into an
+  #   email form (subject, from, to, etc)
+  # your email code goes here
+
+  from = SendGrid::Email.new(email: params[:from])
+  subject = params[:subject]
+  to = SendGrid::Email.new(email: 'sarahrosepainting@gmail.com')
+  content = SendGrid::Content.new(type: 'text/plain', value: params[:content])
+  mail = SendGrid::Mail.new(from, subject, to, content)
+   sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
+
+   response = sg.client.mail._('send').post(request_body: mail.to_json)
+  puts response.status_code
+  puts response.body
+
+  # this redirects to the contact form
+  redirect "/contact"
 end
